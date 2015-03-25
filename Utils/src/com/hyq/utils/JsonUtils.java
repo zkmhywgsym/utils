f551de930942a0c66e7b4a99afbc6ab70e3794a6
Administrator <Administrator@J8GC8MIFGGCYPIM>
2015-03-25 18:33:26 +0800

    utils

--- Utils/src/com/hyq/utils/JsonUtils.java
+++ Utils/src/com/hyq/utils/JsonUtils.java
@@ -1,11 +1,10 @@
 package com.hyq.utils;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.alibaba.fastjson.JSON;
 //fastJson用法
-public class JaonUtils {
+public class JsonUtils {
 	//json转实体
 	public static <T> T jsonToObj(String json,Class<T> t){
 		return JSON.parseObject(json,t);
@@ -16,14 +15,5 @@ public class JaonUtils {
 	}
 	public static <T> List<T> jsonToList(String json,Class<T> className){
 		return JSON.parseArray(json, className);
-	}
-	public static void main(String[] args) {
-		List<Test> list=new ArrayList<Test>();
-		Test test=new Test();
-		list.add(test);
-		test.setName("name");
-		System.out.println(objToJson(list));
-		System.out.println(jsonToList(objToJson(list), Test.class).get(0).getName());
-		System.out.println(jsonToObj(objToJson(test), test.getClass()).getName());
 	}
 }
